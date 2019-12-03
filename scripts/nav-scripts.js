6bccc8d9a427800857d4ba45f10b725161eb970d
Shayna Jamieson <jamieson.shayna@gmail.com>
2019-12-02 17:54:45 -0800

    Finish all layout

--- scripts/nav-scripts.js
+++ scripts/nav-scripts.js
@@ -1,3 +1,12 @@
+/*
+Author: Shayna Jamieson
+File Name: nav-scripts.js
+Last Modified: 12/02/2019
+
+This file contains the scripts that allow the user to be taken to various project
+pages when they click on the buttons associated with each project.
+ */
+
 // when the user clicks on the IDAYDream project button we re-direct
 // them to the website on a new window tab
 $('#daydream-btn').on('click', function() {
@@ -8,4 +17,10 @@ $('#daydream-btn').on('click', function() {
 // them to the website on a new window tab
 $('#resume-btn').on('click', function() {
     window.open('http://sjamieson.greenriverdev.com/305/HTML-Resume/index.html', '_blank');
+});
+
+// when the user clicks on the guest book project button we re-direct
+// them to the website on a new window tab
+$('#guestbook-btn').on('click', function() {
+    window.open('http://sjamieson.greenriverdev.com/305/Guest-Book/guestbook.php', '_blank');
 });
