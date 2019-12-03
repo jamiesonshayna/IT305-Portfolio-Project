6555fe2a8eb26f10cc370ac10ceb5ed1107593de
Shayna Jamieson <jamieson.shayna@gmail.com>
2019-12-02 17:10:33 -0800

    Add projects 1 and 2

---
+++ scripts/nav-scripts.js
@@ -0,0 +1,11 @@
+// when the user clicks on the IDAYDream project button we re-direct
+// them to the website on a new window tab
+$('#daydream-btn').on('click', function() {
+   window.open('http://three-of-a-kind.greenriverdev.com/', '_blank');
+});
+
+// when the user clicks on the resume project button we re-direct
+// them to the website on a new window tab
+$('#resume-btn').on('click', function() {
+    window.open('http://sjamieson.greenriverdev.com/305/HTML-Resume/index.html', '_blank');
+});
